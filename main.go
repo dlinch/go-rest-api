2d5cf4b30733ff88e132aa4f13b59cf1bf955705
dlinch <dlinch33@gmail.com>
2018-02-24 21:36:04 -0700

    added data structs

--- main.go
+++ main.go
@@ -7,7 +7,25 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Person struct {
+	ID         string   `json:"id,omitempty"`
+	First_name string   `json:"first_name,omitempty"`
+	Last_name  string   `json:"last_name,omitempty"`
+	Address    *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
 func main() {
+	people = append(people, Person{ID: "1", First_name: "John", Last_name: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", First_name: "Jane", Last_name: "Doe", Address: &Address{City: "City Y", State: "State Y"}})
+	people = append(people, Person{ID: "3", First_name: "Francis", Last_name: "Sunday"})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/people", GetPeople).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
